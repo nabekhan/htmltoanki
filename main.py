ba65f96e9e2c32308f25c5e3d82ed6ee0efc4192
nabe <nabecite@gmail.com>
2024-11-20 23:43:49 -0700

    Provide error with incorrect url

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ logging.basicConfig(level=logging.DEBUG)
 
 def deckcreate(username, password, deck):
 
+    if "printdeck" not in deck:
+        raise ValueError("The URL is not valid. Did you provide the print url as instructed? Ex: https://cards.ucalgary.ca/printdeck/882?bag_id=77")
 
-    # Define URLs
+        # Define URLs
     login_url = "https://cards.ucalgary.ca/login"
     collection_url = "https://cards.ucalgary.ca/collection"
 
